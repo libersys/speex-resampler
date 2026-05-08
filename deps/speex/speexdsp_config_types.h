89554ed5bc55acd601c10ca69fee1b09de405fc5
Dioris Moreno <dmoreno@libersys.io>
2026-05-08 17:02:20 -0500

    speexdsp_config_types.h was missing

---
+++ deps/speex/speexdsp_config_types.h
@@ -0,0 +1,12 @@
+/* speexdsp_config_types.h - generated substitute for autoconf output */
+#ifndef __SPEEXDSP_CONFIG_TYPES_H__
+#define __SPEEXDSP_CONFIG_TYPES_H__
+
+#include <stdint.h>
+
+typedef int16_t spx_int16_t;
+typedef uint16_t spx_uint16_t;
+typedef int32_t spx_int32_t;
+typedef uint32_t spx_uint32_t;
+
+#endif /* __SPEEXDSP_CONFIG_TYPES_H__ */
